82e08790179acb5d4075a5f41b536e30b7ea3605
g4brieljs <gabrieljimenez690@gmail.com>
2019-07-10 21:17:24 -0400

    Organizando code

--- Number-String.js
+++
@@ -1,8 +0,0 @@
-// Convert from String to a Number
-// This is used to for know a postal code or Card code
-
-let cp;
-cp = 90210;
-cp = String(cp);
-
-console.log(cp.length); // Length this is a Method to knows if it is a how to characters

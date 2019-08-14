be354dbcfc22c643a8749e5f05996718056f128e
g4brieljs <gabrieljimenez690@gmail.com>
2019-08-13 23:22:33 -0400

    Finish project Simulator email

--- 6-SegundoProyecto(Email)/email/js/app.js
+++ 6-SegundoProyecto(Email)/email/js/app.js
@@ -4,6 +4,7 @@ const email = document.getElementById('email');
 const asunto = document.getElementById('asunto');
 const mensaje = document.getElementById('mensaje');
 const btnEnviar = document.getElementById('enviar');
+const formularioEnviar = document.getElementById('enviar-mail');
 
 // Event listener
 
@@ -11,12 +12,15 @@ eventListeners();
 
 function eventListeners(){
     //Cuando inicie la aplicacion\
-    document.addEventListener('DOMContendLoaded', inicioApp);
+    document.addEventListener('DOMContentLoaded', inicioApp);
 
     // Campos del formulario
     email.addEventListener('blur', validarCampo);
     asunto.addEventListener('blur', validarCampo);
     mensaje.addEventListener('blur', validarCampo);
+
+    // Boton de enviar
+    formularioEnviar.addEventListener('submit', enviarEmail);
 }
 
 // Functions
@@ -24,7 +28,7 @@ function eventListeners(){
 function inicioApp(){
     // Deshabilitar el envio cuando cargue la página
 
-    btnEnviar.disable = true;
+    btnEnviar.disabled = true;
 }
 
 
@@ -35,17 +39,70 @@ function validarCampo(){
     // Validar la longitu del texto y que no este vacío
     validarLongitud(this);
 
+    // Validar el Email
+    if(this.type === 'email'){
+        validarEmail(this);
+    }
+
+    let errores = document.querySelectorAll('.error');
+
+
     if(email.value !== '' && asunto.value !== '' && mensaje.value !== ''){
-        if(errores.lenght ===0){
-            btnEnviar.disable = false;
+        if(errores.length === 0){
+            btnEnviar.disabled = false;
         }
     }
 }
 
-function validarLongitud(campo){
-    console.log(campo.value.lenght);
+// Cuando se envia el correo
+function enviarEmail(e){
+    // Spinner al presionar enviar
+    const spinnerGif = document.querySelector('#spinner');
+    spinnerGif.style.display = 'block';
 
-    if(campo.value.lenght > 0){
+    //Gif que envia el email
+    const enviado = document.createElement('img');
+    enviado.src = 'img/mail.gif';
+    enviado.style.display = 'block';
+
+    // Ocultar Spinner y mostrar GIf de enviado
+
+    setTimeout(function(){
+        spinnerGif.style.display = 'none';
+
+        document.querySelector('#loaders').appendChild(enviado);
+
+        setTimeout(function(){
+            enviado.remove();
+            formularioEnviar.reset();
+        }, 4000);
+    },3000);
+
+    e.preventDefault();
+
+    
+
+}
+
+// Con esta funcion podemos validar la longitud de los input y saber si tiene letra o no
+
+function validarLongitud(campo){
+    console.log(campo.value.length);
+
+    if(campo.value.length > 0){
+        campo.style.borderBottomColor = 'green';
+        campo.classList.remove('error');
+    }else {
+        campo.style.borderBottomColor = 'red';
+        campo.classList.add('error');
+    }
+}
+
+// Vamos a validar que es un email
+
+function validarEmail(campo){
+    const mensaje = campo.value;
+    if(mensaje.indexOf('@') !== -1){
         campo.style.borderBottomColor = 'green';
         campo.classList.remove('error');
     }else {
